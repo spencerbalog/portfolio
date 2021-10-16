3a018c3b2d8c79bca60dfe29e8af98efe1942d6c
M Balog <mbalog@Ms-MacBook-Pro.local>
2021-10-15 21:38:23 -0400

    remove <a> from app.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Beep-beep boop-bop.
+          Beep-beep boop-bop
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
