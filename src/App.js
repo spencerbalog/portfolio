f7d7c8a1727d64f56926c0b9942aea301f8213ee
M Balog <mbalog@Ms-MacBook-Pro.local>
2021-10-16 19:00:48 -0400

    add components folder within src folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Beep-beep boop-bop, this is a WIP.
-        </p>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <About/>
+    </main>
   );
 }
 
-export default App;
